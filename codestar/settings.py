05cb53168b3db3b42584b631793c1835c9009abb
Matthew Aitken <matt@itken.net>
2024-09-20 08:58:18 +0000

    Add secret ket environment variable

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=f9-f=o)3@#4hobub%r07j@vll_mllm17+^x@lgkvq=6&wlqn!'
+# SECRET_KEY = 'django-insecure-=f9-f=o)3@#4hobub%r07j@vll_mllm17+^x@lgkvq=6&wlqn!'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False

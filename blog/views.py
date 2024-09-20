b806dac11672062734817169c28913bcc5e20e27
Matthew Aitken <matt@itken.net>
2024-09-20 13:18:02 +0000

    Add base.html and set up templates

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,6 @@ from .models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=0)
+    queryset = Post.objects.all()
+    template_name = 'blog/index.html'
+    paginate_by = 6
